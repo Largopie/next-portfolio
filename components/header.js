ed46595512e02a523bad2f3ed5a62e9651d706b0
Largopie <106071687+Largopie@users.noreply.github.com>
2023-09-21 02:31:31 +0900

    ui: dark mode 적용 및 css 수정

--- components/header.js
+++ components/header.js
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import DarkModeToggleButton from './dark-mode-toggle';
 
 export default function Header() {
   return (
@@ -16,11 +17,7 @@ export default function Header() {
             <Link href="/projects" className="mr-5 hover:text-gray-900">프로젝트</Link>
             <Link href="https://open.kakao.com/o/sw0yViIf" className="mr-5 hover:text-gray-900">연락하기</Link>
           </nav>
-          <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Button
-            <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
-              <path d="M5 12h14M12 5l7 7-7 7"></path>
-            </svg>
-          </button>
+          <DarkModeToggleButton />
         </div>
       </header>
     </>
--- components/home/hero.js
+++ components/home/hero.js
@@ -12,7 +12,7 @@ export default function Hero() {
           같지 두손을 따뜻한 오직 그들의 것이다. 많이 봄바람을 사랑의 부패를 유소년에게서 이상의 이상은 그리하였는가? 풍부하게 가치를 기쁘며, 밝은 것이다. 구할 천지는 청춘의 위하여 이것이다. 풍부하게 때까지 더운지라 지혜는 맺어, 유소년에게서 것이다. 온갖 피가 미인을 청춘의 약동하다. 많이 풀이 이상을 할지니, 사람은 무엇을 우리 말이다. 방지하는 내려온 것은 부패를 생생하며, 전인 영락과 위하여서. 반짝이는 이것은 오직 인간의 듣는다.
         </p>
         <div className="flex justify-center">
-          <Link href="/projects" className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+          <Link href="/projects" className="btn-project">
             프로젝트 보러가기
           </Link>
         </div>
