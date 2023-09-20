0cce5386a16c264a6848b2414075897ad25b28d5
Largopie <106071687+Largopie@users.noreply.github.com>
2023-09-20 21:33:40 +0900

    build: tailwind 초기 세팅

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>홈 입니다.</h1>
+      <h1 className="text-3xl font-bold underline">
+        홈 입니다.
+      </h1>
     </Layout>
   )
 }
