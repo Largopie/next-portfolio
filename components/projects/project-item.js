3650ad5e40d27af134f130b8f531c51197254b60
Largopie <106071687+Largopie@users.noreply.github.com>
2023-09-21 13:06:42 +0900

    reafctor: 받아온 데이터 추가 및 css 수정

---
+++ components/projects/project-item.js
@@ -0,0 +1,35 @@
+import Image from 'next/image'
+
+export default function ProjectItem({ data }) {
+
+  const title = data.properties.ProjectName.title[0].plain_text
+  const description = data.properties.Description.rich_text[0].plain_text
+  const github = data.properties.GitHub.url
+  const imgSrc = data.cover.file?.url
+  const tags = data.properties.Tags.multi_select
+
+  return (
+    <div className="project-card">
+      <Image
+        className="rounded-xl"
+        src={imgSrc}
+        alt="cover image"
+        width="100"
+        height="60"
+        layout="responsive"
+        objectFit="cover"
+        quality={100}
+      />
+      <div className="p-4 flex flex-col">
+        <h1 className="text-2xl font-bold">{title}</h1>
+        <h1 className="mt-4 text-xl">{description}</h1>
+        <a href={github}>깃허브 바로가기</a>
+        <div className="flex items-start mt-2">
+          {tags.map((aTag) => (
+            <h1 className="px-2 py-1 mr-2 rounded-md bg-sky-200 dark:bg-sky-700 w-30" key={aTag.id}>{aTag.name}</h1>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
--- pages/projects.js
+++ pages/projects.js
@@ -1,23 +1,30 @@
 import Layout from "@/components/layout";
 import Head from "next/head";
 import { TOKEN, DATABASE_ID } from "../config"
+import ProjectItem from "@/components/projects/project-item";
 
 export default function Projects({ projects }) {
 
   return (
     <Layout>
-      <Head>
-        <title>송재석 포트폴리오</title>
-        <meta name="description" content="First Next.js Project" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <div className="flex flex-col items-center justify-center min-h-screen px-6 mb-10">
+        <Head>
+          <title>송재석 포트폴리오</title>
+          <meta name="description" content="First Next.js Project" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <h1>총 프로젝트 : { projects.results.length }</h1>
-
-      {projects.results.map((aProject) => (
-        <h1>{aProject.properties.ProjectName.title[0].plain_text}</h1>
-      ))}
+        <h1 className="text-4xl font-bold sm:text-6xl">
+          총 프로젝트 : 
+          <span class="pl-4 text-blue-500">{projects.results.length}</span>
+        </h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 py-10 m-6 gap-8 w-full">
+          {projects.results.map((aProject) => (
+            <ProjectItem key={aProject.id} data={aProject} />
+          ))}
+        </div>
+      </div>
     </Layout>
   );
 }
@@ -42,7 +49,7 @@ export async function getStaticProps() {
       page_size: 100
     })
   };
-  
+
   const res = await fetch(`https://api.notion.com/v1/databases/${DATABASE_ID}/query`, options)
   const projects = await res.json()
 
@@ -53,6 +60,6 @@ export async function getStaticProps() {
   console.log(`projectNames: ${projectNames}`)
 
   return {
-    props: {projects},
+    props: { projects },
   }
 }
