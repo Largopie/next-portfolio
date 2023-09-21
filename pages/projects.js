68b8569ba874dd598edbd0e541e80c84b16b6415
Largopie <106071687+Largopie@users.noreply.github.com>
2023-09-21 11:45:31 +0900

    feat: notion api 연동 및 브라우저 정상 동작 하는지 확인

--- pages/projects.js
+++ pages/projects.js
@@ -1,7 +1,9 @@
 import Layout from "@/components/layout";
 import Head from "next/head";
+import { TOKEN, DATABASE_ID } from "../config"
 
-export default function Projects() {
+export default function Projects({ projects }) {
+
   return (
     <Layout>
       <Head>
@@ -10,7 +12,47 @@ export default function Projects() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>프로젝트</h1>
+
+      <h1>총 프로젝트 : { projects.results.length }</h1>
+
+      {projects.results.map((aProject) => (
+        <h1>{aProject.properties.ProjectName.title[0].plain_text}</h1>
+      ))}
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+
+  const options = {
+    method: 'POST',
+    headers: {
+      'accept': 'application/json',
+      'Notion-Version': '2022-06-28',
+      'content-type': 'application/json',
+      'authorization': `Bearer ${TOKEN}`
+    },
+    body: JSON.stringify({
+      sorts: [
+        {
+          "property": "ProjectName",
+          "direction": "ascending"
+        }
+      ],
+      page_size: 100
+    })
+  };
+  
+  const res = await fetch(`https://api.notion.com/v1/databases/${DATABASE_ID}/query`, options)
+  const projects = await res.json()
+
+  const projectNames = projects.results.map((aProject) => (
+    aProject.properties.ProjectName.title[0].plain_text
+  ))
+
+  console.log(`projectNames: ${projectNames}`)
+
+  return {
+    props: {projects},
+  }
 }
