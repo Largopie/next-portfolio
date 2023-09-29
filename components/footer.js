0ec6576c6098c07b42fcc7c0b9440864291cbfad
Largopie <106071687+Largopie@users.noreply.github.com>
2023-09-29 18:46:46 +0900

    ui: 깃허브 아이콘 추가 및 미사용 아이콘 일부 삭제

--- components/footer.js
+++ components/footer.js
@@ -5,15 +5,13 @@ export default function Footer() {
     <>
       <footer className="body-font">
         <div>
-          <div className="container px-5 py-6 mx-auto flex items-center sm:flex-row flex-col">
-            <Link href="/" className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+          <div className="container flex flex-col items-center px-5 py-6 mx-auto sm:flex-row">
+            <Link href="/" className="flex items-center justify-center font-medium text-gray-900 title-font md:justify-start">
               <span className="ml-3 text-xl">송재석 포트폴리오</span>
             </Link>
-            <p className="text-sm text-gray-500 sm:ml-6 sm:mt-0 mt-4">© 2020 Tailblocks —
-              <a href="https://twitter.com/knyttneve" rel="noopener noreferrer" className="text-gray-600 ml-1" target="_blank">@knyttneve</a>
-            </p>
-            <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-              <a className="text-gray-500">
+            <p className="mt-4 text-sm text-gray-500 sm:ml-6 sm:mt-0">Frontend Engineer</p>
+            <span className="inline-flex justify-center mt-4 sm:ml-auto sm:mt-0 sm:justify-start">
+              {/* <a className="text-gray-500">
                 <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
                   <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
                 </svg>
@@ -27,6 +25,11 @@ export default function Footer() {
                 <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
                   <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
                   <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+                </svg>
+              </a> */}
+              <a href="https://github.com/Largopie" className="ml-3 text-gray-500">
+                <svg fill="currentColor" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="0" className="w-6 h-6" viewBox="0 0 24 24">
+                  <path fill-rule="evenodd" d="M10 .333A9.911 9.911 0 0 0 6.866 19.65c.5.092.678-.215.678-.477 0-.237-.01-1.017-.014-1.845-2.757.6-3.338-1.169-3.338-1.169a2.627 2.627 0 0 0-1.1-1.451c-.9-.615.07-.6.07-.6a2.084 2.084 0 0 1 1.518 1.021 2.11 2.11 0 0 0 2.884.823c.044-.503.268-.973.63-1.325-2.2-.25-4.516-1.1-4.516-4.9A3.832 3.832 0 0 1 4.7 7.068a3.56 3.56 0 0 1 .095-2.623s.832-.266 2.726 1.016a9.409 9.409 0 0 1 4.962 0c1.89-1.282 2.717-1.016 2.717-1.016.366.83.402 1.768.1 2.623a3.827 3.827 0 0 1 1.02 2.659c0 3.807-2.319 4.644-4.525 4.889a2.366 2.366 0 0 1 .673 1.834c0 1.326-.012 2.394-.012 2.72 0 .263.18.572.681.475A9.911 9.911 0 0 0 10 .333Z" clip-rule="evenodd" />
                 </svg>
               </a>
               <a className="ml-3 text-gray-500">
