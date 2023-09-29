bb264a68475b3f76ed98d0b058367f9f1ca6827e
Largopie <106071687+Largopie@users.noreply.github.com>
2023-09-29 22:46:43 +0900

    chore: 메인 페이지 문구 재수정

--- components/home/hero.js
+++ components/home/hero.js
@@ -9,9 +9,10 @@ export default function Hero() {
           <br className="hidden lg:inline-block" />
         </h1>
         <p className="mb-8 leading-relaxed">
-        편리한 웹 서비스 제공해 이용자를 매력적으로 끌어당기는 <br />🧲<strong>자석</strong>같은 개발자가 되기 위해 배움의 계단을 한칸한칸 밟아나가는 중입니다. <br/>
-          매년 새로운 기술이 등장하고 발전하는 현대에서 끊임없이 성장하며 미래 산업의 중심에서 개발을 이어나가고 싶은 강한 열망을 품고 있는 개발자입니다.
-          새로운 언어와 기술을 신속하게 습득하고 이를 실전에 빠르게 적용하는 능력을 갖추고자 합니다.
+        편리한 웹 서비스를 제공해 이용자를 매력적으로 끌어당기는 <br />
+        <strong>🧲자석</strong> 같은 개발자가 되기 위해 배움의 계단을 한칸 한칸 밟아나가는 중입니다.<br /><br />
+        매년 새로운 기술이 등장하고 발전하는 현대에서 끊임없이 성장하며 미래 산업의 중심에서 개발을 이어 나가고 싶은 열망을 가진 개발자입니다.
+        새로운 언어와 기술을 신속하게 습득하고 이를 실전에 빠르게 적용하는 능력을 갖추고자 합니다.
         </p>
         <div className="flex justify-center">
           <Link href="/projects" className="btn-project">
